5072d7b277642d7b8799b61ef10e64602742d6a7
alexanderxp <alexanderxp@ukr.net>
2017-03-15 11:24:46 +0200

    DZ.09.03.2017

---
+++ DZ_09.03.2017/Skript_prostih_chisel.php
@@ -0,0 +1,20 @@
+<?php
+
+$arr = array();
+ 
+for ($i = 2; $i <= 100; $i++)
+{ 
+    $flag = true;
+
+    for ($j = 2; $j <= $i/2; $j++)
+
+        if( $i % $j == 0)
+            $flag = false;
+        
+    if($flag)
+        $arr[] = $i;
+}
+var_dump($arr);
+   
+
+?>
